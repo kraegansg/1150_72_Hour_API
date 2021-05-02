221d9b0a73f6166577b9671d19edb1d05cb419e2
ksgraves <kraegan.s.g@gmail.com>
2021-05-02 18:51:11 -0400

    pushing models layer, code from Greg

---
+++ 72.Hour.Models/CommentCreate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72.Hour.Models
+{
+    class CommentCreate
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+    }
+}
---
+++ 72.Hour.Models/ReplyCreate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _72.Hour.Models
+{
+    public class ReplyCreate
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public Guid OwnerId { get; set; }
+    }
+}
